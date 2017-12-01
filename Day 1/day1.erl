74303f4faf956d72f817d89412471d38f22bc342
aaruel <wmakit88@gmail.com>
2017-12-01 04:07:40 -0500

    Fix spacing on day1.erl

--- Day 1/day1.erl
+++ Day 1/day1.erl
@@ -2,27 +2,27 @@
 -export([start_parsing/1, test/1]).
 
 two_num_equality([A, B]) -> 
-	A == B.
+    A == B.
 
 recurse_numbers([_]) ->
-	0;
+    0;
 recurse_numbers(Data) ->
-	{Comparator, _} = lists:split(2, Data),
-	{Reference, _} = lists:split(1, Comparator),
-	case two_num_equality(Comparator) of
-		true -> list_to_integer(Reference) + recurse_numbers(tl(Data));
-		false -> 0 + recurse_numbers(tl(Data))
-	end.
+    {Comparator, _} = lists:split(2, Data),
+    {Reference, _} = lists:split(1, Comparator),
+    case two_num_equality(Comparator) of
+        true -> list_to_integer(Reference) + recurse_numbers(tl(Data));
+        false -> 0 + recurse_numbers(tl(Data))
+    end.
 
 make_circular(List) ->
-	[First | _] = List,
-	List ++ [First].
+    [First | _] = List,
+    List ++ [First].
 
 test(List) ->
-	recurse_numbers(make_circular(List)).
+    recurse_numbers(make_circular(List)).
 
 start_parsing(Filepath) ->
-	case file:read_file(Filepath) of
-		{ok, Data} -> recurse_numbers(make_circular(binary_to_list(Data)));
-		_ -> {err}
-	end.
+    case file:read_file(Filepath) of
+        {ok, Data} -> recurse_numbers(make_circular(binary_to_list(Data)));
+        _ -> {err}
+    end.
